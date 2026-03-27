9151396d99f2d703f2c1c6ea040ba0778711d5ee
Saad-Manda <manda_2401ai28@iitp.ac.in>
2026-03-27 21:24:37 +0530

    Initialized socket server

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -1,30 +1,17 @@
 import dotenv from 'dotenv';
-import express from "express";
-import cors from 'cors';
 import connectDB from './db.connect.js';
+import { startServer } from './server.js';
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || process.env.PORT || 5000;
 
-async function startServer() {
+const initApp = async () => {
   await connectDB();
-
-  app.use(express.urlencoded({ limit: "40kb", extended: true }));
-  app.use(express.json());
-  app.use(cors());
-
-  app.get("/", (req, res) => {
-    res.send("API running");
-  });
-
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  await startServer({ port });
 }
 
-startServer().catch(err => {
+initApp().catch(err => {
   console.error("Failed to start server:", err);
   process.exit(1);
 });
---
+++ apps/backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import { createWorker } from "./utils/workerPool.js";
+
+export const startServer = async ({ port }) => {
+
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server);
+
+    app.use(express.urlencoded({ limit: "40kb", extended: true }));
+    app.use(express.json());
+    app.use(cors());
+
+    await createWorker();
+
+    io.on("connection", (socket) => {
+    console.log(`Client connected: ${socket.id}`);
+    });
+
+    await new Promise((resolve) => {
+        server.listen(port, resolve);
+    });
+
+    console.log(`Server listening on ${port}`);
+
+    return { app, server, io };
+}
