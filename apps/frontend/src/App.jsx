2c41bf880debd39f80bb553f340e5500af58af53
Rameshwar1302 <ramdudhate1302@gamil.com>
2026-03-28 19:37:45 +0530

    Added Root.jsx to navigate the user by authentication

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -5,6 +5,8 @@ import { Toaster } from "react-hot-toast";
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import Dashboard from './components/Dashboard.jsx'
+import  Root from './utils/Root.jsx'
+import ProtectedRoutes from './utils/ProtectedRoutes.jsx';
 
 function App() {
   
@@ -14,10 +16,20 @@ function App() {
     <Toaster position="bottom-left" reverseOrder={false} />
     <Router>
       <Routes>
-        <Route path='/' element={<h1>Zynk</h1>} />
+         <Route path='/' element={<Root />}/>
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>} />
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/dashboard' 
+          element={
+            <ProtectedRoutes>
+              <Dashboard/>
+            </ProtectedRoutes>        
+          }/>
+
+
+
+
+        <Route path='/unauthorized' element={<p className='font-bold text-3xl mt-20 ml-20 '>UnAuthorized Path</p>}/>
       </Routes>
     </Router>
       
