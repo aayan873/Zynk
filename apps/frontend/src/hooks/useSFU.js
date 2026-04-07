c17a95805b5a653f226464f9da06f15700fed982
Minhaj <mdminhaju29@gmail.com>
2026-04-08 03:16:24 +0530

    added useCallBack in useSFU

--- apps/frontend/src/hooks/useSFU.js
+++ apps/frontend/src/hooks/useSFU.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState, useCallback } from "react"
 import { Device } from "mediasoup-client"
 
 export const useSFU = (socket, roomId) => {
