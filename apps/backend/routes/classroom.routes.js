7f394be6a6bbb073a6e7cb1ffa043ff228f65d86
Saad-Manda <manda_2401ai28@iitp.ac.in>
2026-04-12 11:42:12 +0530

    Added enroll endpoint to the router

--- apps/backend/routes/classroom.routes.js
+++ apps/backend/routes/classroom.routes.js
@@ -7,13 +7,14 @@ import {
   deleteClassroom,
   getClassroom,
   getAllClassrooms,
+  enrollClassroom,
 } from '../controllers/classroom.controller.js';
 
 const router = express.Router();
 
 router.get('/', requireAuth, getAllClassrooms);
 router.get('/:id', requireAuth, getClassroom);
-
+router.post('/:id/enroll', requireAuth, enrollClassroom);
 router.post('/', requireAuth, isTeacher, createClassroom);
 router.patch('/:id', requireAuth, isTeacher, updateClassroom);
 router.delete('/:id', requireAuth, isTeacher, deleteClassroom);
