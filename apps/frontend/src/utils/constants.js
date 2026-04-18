18dbeedd6859332235b21f008a58a2d5a2b75258
Saad-Manda <manda_2401ai28@iitp.ac.in>
2026-04-18 23:09:37 +0530

    Added hard coded branches and degrees

---
+++ apps/frontend/src/utils/constants.js
@@ -0,0 +1,22 @@
+export const BRANCHES = [
+    { value: 'Computer Sciences Engineering', label: 'Computer Sciences Engineering (CS)' },
+    { value: 'Artificial Intelligence and Data Sciences', label: 'Artificial Intelligence and Data Sciences (AI)' },
+    { value: 'Mathematics and Computing', label: 'Mathematics and Computing (MC)' },
+    { value: 'Electronics & Electrical Engineering', label: 'Electronics & Electrical Engineering (EE)' },
+    { value: 'Electronics and Communication Engineering', label: 'Electronics and Communication Engineering (ECE)' },
+    { value: 'Civil Engineering', label: 'Civil Engineering (CE)' },
+    { value: 'Chemical Science and Technology', label: 'Chemical Science and Technology (CT)' },
+    { value: 'Chemical and Biotechnology', label: 'Chemical and Biotechnology (CB)' },
+    { value: 'Engineering Physics', label: 'Engineering Physics (PH)' },
+    { value: 'Mechanical Engineering', label: 'Mechanical Engineering (ME)' },
+    { value: 'Metallurgy and Material Sciences', label: 'Metallurgy and Material Sciences (MM)' },
+    { value: 'Economics', label: 'Economics (ES)' }
+];
+
+export const PROGRAMMES = [
+    { value: 'BTech (4 year)', label: 'BTech (4 year)' },
+    { value: 'BTech + Mtech (5 yr dual degree)', label: 'BTech + Mtech (5 yr dual degree)' },
+    { value: 'BTech + MBA (5 yr dual degree)', label: 'BTech + MBA (5 yr dual degree)' },
+    { value: 'Mtech (2 years)', label: 'Mtech (2 years)' },
+    { value: 'PhD', label: 'PhD' }
+];
